6c6008a0a2b1516f50f1a0f2c1cabf994beac1a9
Dominic Portolese <102822047+DominicPortolese@users.noreply.github.com>
2022-08-19 13:09:44 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -6,20 +6,20 @@ other important info: none
  */
 public class Driver{
     public static  void main(String[] args)throws CloneNotSupportedException{
-        //    System.out.println("Hi");
+
         Money money1 = new Money(420, 69);
         Money money2 = new Money(20,32);
         Date date1 = new Date(3,28,2021);
         Date date2 = new Date(7,13, 2022);
 
-        Money badMoney = new Money(-9,0);
-        Date badDate = new Date(50,51,52);
+    //    Money badMoney = new Money(-9,0);
+   //     Date badDate = new Date(50,51,52);
 
         Bill bill1 = new Bill(money1, date1, "Robinhood");
         Bill bill2 = new Bill(money2, date2, "Spotify");
         Bill bill3 = new Bill(money1, date2, "Safeway");
         Bill bill4 = new Bill(money2, date1, "Amazon");
-        Bill badBill = new Bill(badMoney,badDate, "bitCoin");
+    //    Bill badBill = new Bill(badMoney,badDate, "bitCoin");
 
         ExpenseAccount expenses = new ExpenseAccount();
 
@@ -27,7 +27,7 @@ public class Driver{
         expenses.append(bill2);
         expenses.append(bill3);
         expenses.append(bill4);
-        expenses.append(badBill);
+     //   expenses.append(badBill);
 
         ((Bill)(expenses.get(expenses.indexOfObject(bill2)))).setPaid(date1);
 
@@ -41,7 +41,6 @@ public class Driver{
         //  expenses.append(new Bill(money1, date1, "Spotify"));
 
         expenses.sortExpenses();
-        // System.out.println("\nBack into the driver, after sort before print\n");
         System.out.println(  expenses.toString());
         System.out.println("Total Due: $"+expenses.getTotal());
     }
